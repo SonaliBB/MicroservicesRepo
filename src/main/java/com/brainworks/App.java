a6e13b5ce59835ffe687ac14150ef149853e1650
Sonali <sonalibhandalkar99@gmail.com>
2026-08-22 01:34:41 +0530

    initial project setup

---
+++ src/main/java/com/brainworks/App.java
@@ -0,0 +1,17 @@
+package com.brainworks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Git World!" );
+
+        System.out.println("woking on project");
+    }
+
+
+}
