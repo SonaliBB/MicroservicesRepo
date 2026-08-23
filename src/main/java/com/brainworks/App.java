f1622ce14f20ca647ae4006f40c59aca69d22528
Sonali <sonalibhandalkar99@gmail.com>
2026-08-23 19:43:19 +0530

    created a

--- src/main/java/com/brainworks/App.java
+++ src/main/java/com/brainworks/App.java
@@ -6,32 +6,11 @@ package com.brainworks;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Git World!" );
+    public static void main( String[] args ) {
 
-        System.out.println("woking on project");
-        System.out.println("Learning Git step by step");
-        System.out.println("This change belongs to feature-message");
-
-        System.out.println("doing by self");
         int a=20;
-        System.out.println(a+a);
+        System.out.println(a);
 
-        System.out.println("changed from subbranch");
-
-        System.out.println(a*a);
-        
-        int b=2;
-        System.out.println(b);
-
-           String s="sona";
-           System.out.println(s);
-
-        //learning git conflict
-         System.out.println("from github rising conflict  ");
-         System.out.println(s);
     }
-
 
 }
