bdefa41ea1def8fe5c5bb03c7e4525ba8a00bc40
Sonali <sonalibhandalkar99@gmail.com>
2026-08-24 18:19:07 +0530

    Revert "Add code to update.java"
    This reverts commit 19e3561b936fcbfbeeb7f863fc776eaf5e71fa5c.

--- src/main/java/com/brainworks/App.java
+++ src/main/java/com/brainworks/App.java
@@ -4,10 +4,12 @@ package com.brainworks;
  * Hello world!
  *
  */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello Git");
-    }
+public class App 
+{
+    public static void main( String[] args )
+    {
+   System.out.println("hello world");
+}
 }
 
 
