2cde30a66f7e6dcb3853ef13459be175c17e1c43
Sonali <sonalibhandalkar99@gmail.com>
2026-08-24 15:33:03 +0530

    updated in b1

--- src/main/java/com/brainworks/App.java
+++ src/main/java/com/brainworks/App.java
@@ -11,7 +11,11 @@ public class App
 
         System.out.println("Hello World");
         System.out.println("new from branch b1");
-        
+
+        int  add=10;
+        System.out.println("added var"+add);
+
+        System.out.println("In b1");
     }
 
 
